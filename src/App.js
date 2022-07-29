3b17aa4658dde88b01f27366ea5c11581ac36fdd
drhcode <drhcode1@gmail.com>
2022-07-30 01:14:39 +0200

    weatherApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+import Descriptions from "./components/Descriptions";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
 
 function App() {
+  const [city, setCity] = useState("Korce");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotBg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
+
+      //dynamic bg
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp < threshold) setBg(coldBg);
+      else setBg(hotBg);
+    };
+    fetchWeatherData();
+  }, [units, city]);
+
+  const handUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+    const isCelsius = currentUnit === "C";
+    button.innerText = isCelsius ? "°F" : "°C";
+    setUnits(isCelsius ? "metric" : "imperial");
+  };
+
+  // const enterKeyPressed = (e) => {
+  //   if (e.keyCode === 13) {
+  //     setCity(e.currentTarget.value);
+  //     e.currentTarget.blur();
+  //   }
+  // };
+
+  // const changeCity = (e) => {
+  //   city(e.currentTarget.value);
+  // };
+
+  const buttonChangeCity = (e) => {
+    setCity(e.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                value={city}
+                onChange={buttonChangeCity}
+                // onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              <button onClick={handUnitsClick}>°C</button>
+            </div>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="" />
+                <h3>{weather.Description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+            {/* description*/}
+            <Descriptions weather={weather} units={units} />
+            <h5 style={{ color: "white" }}>Made by DRHCode</h5>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
